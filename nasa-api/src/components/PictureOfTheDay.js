7156c163db342f38011b144d2c72d22c65f0a2d6
tomg <goresthomasj@johndeere.com>
2021-02-22 09:56:07 -0600

    AOPD -1

--- nasa-api/src/components/PictureOfTheDay.js
+++ nasa-api/src/components/PictureOfTheDay.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 
+const nasaAPIKey = "dKN5xKkmeDWUEQEMaF13fpONtxkimP7vr0hPgjC7"
+
 class PictureOfTheDay extends Component {
     constructor(props) {
         super(props);
@@ -10,10 +12,19 @@ class PictureOfTheDay extends Component {
         }
     }
 
+    componentDidMount = async () => {
+        const pictureData = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${nasaAPIKey}`)
+        console.log(pictureData.data);
+        this.setState({
+          picture: pictureData.data
+        })
+      }
+
     render() {
         return(
             <div>
                 Picture
+                <img className="image" src={this.state.picture.url}/>  
             </div>
         )
     }
