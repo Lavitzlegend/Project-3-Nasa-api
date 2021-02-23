ff462b61cc1eccc1d50361e0be9b725fecf4b6d6
tomg <goresthomasj@johndeere.com>
2021-02-23 17:41:02 -0600

    ImageAndVideoLib -6

--- nasa-api/src/components/ImageAndVideoLib.js
+++ nasa-api/src/components/ImageAndVideoLib.js
@@ -15,14 +15,14 @@ class ImageAndVideoLib extends Component {
     }
 
     componentDidMount = async () => {
-        const imageVideoData = await axios.get(`https://images-api.nasa.gov/search?q=voyager&media_type=image&year_start=1980&year_end=1981`)
+        const imageVideoData = await axios.get(`https://images-api.nasa.gov/search?q=voyager&year_start=1980&year_end=1981`)
         const imageVideoDataMeta = await axios.get(`https://images-api.nasa.gov/asset/172_ISS-Slosh?api_key=${nasaAPIKey}`)
         console.log("test");
         console.log(imageVideoData);
         console.log(imageVideoDataMeta);
           this.setState({
             imageVideo: imageVideoData.data.collection.items,
-            imageVideo: imageVideoDataMeta.data.collection.items,
+            imageVideoMeta: imageVideoDataMeta.data.collection.items,
             apiDataLoaded: true
         })
       }
@@ -33,8 +33,11 @@ class ImageAndVideoLib extends Component {
             <div className="imgvidlib">
                 <h1 className="compHeader">Images And Videos</h1>
                 {this.state.apiDataLoaded && this.state.imageVideo.map(image => (
-                    <img className="podPic" src={image.links[0].href}/>))
-                }
+                    <div>
+                    <img className="podPic" src={image.links[0].href}/>
+                    <p className="podDesc"> {image.data[0].description}</p>
+                    </div>
+                ))}
                 
             </div>
         )
