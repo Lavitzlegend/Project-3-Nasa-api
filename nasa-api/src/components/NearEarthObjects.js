77204433bc3e9ccab15bb14a6783628e5056e4e3
anace-m <maldonadoanaceciliac@johndeere.com>
2021-02-22 18:27:14 -0600

    Formatted decimals

--- nasa-api/src/components/NearEarthObjects.js
+++ nasa-api/src/components/NearEarthObjects.js
@@ -23,24 +23,25 @@ class NearEarthObjects extends Component {
         })
     }
 
-    render() {        
+    render() {      
         return(
             <div>
-                <h2>Date: Feb 21st 2021</h2>
+                <h1 className="compHeader">Near Earth Objects</h1>
+                <h2 className="compSubHeader">Date: Feb 21st 2021</h2>
                 <div>{this.state.apiDataLoaded ? this.state.objects['2021-02-21'].map((object, index) => (
                     <div key={`Neo ` + index}>
                         <p><strong>Object {index + 1}: {object.name}</strong></p>
-                        <p>Average Diameter: {(object.estimated_diameter.feet.estimated_diameter_min + object.estimated_diameter.feet.estimated_diameter_max)/2} ft</p>
+                        <p>Average Diameter: {((object.estimated_diameter.feet.estimated_diameter_min + object.estimated_diameter.feet.estimated_diameter_max)/2).toFixed(2)} ft</p>
                         <p>Run For Our Lives? = {`${object.is_potentially_hazardous_asteroid}`}</p>
                         <a href={object.nasa_jpl_url} >More Info</a>
                     </div>
                 )) : "Loading..."}</div>
 
-                <h2>Date: Feb 20th 2021</h2>
+                <h2 className="compSubHeader">Date: Feb 20th 2021</h2>
                 <div>{this.state.apiDataLoaded ? this.state.objects['2021-02-20'].map((object, index) => (
                     <div key={`Neo ` + index}>
                         <p><strong>Object {index + 1}: {object.name}</strong></p>
-                        <p>Average Diameter: {(object.estimated_diameter.feet.estimated_diameter_min + object.estimated_diameter.feet.estimated_diameter_max)/2} ft</p>
+                        <p>Average Diameter: {((object.estimated_diameter.feet.estimated_diameter_min + object.estimated_diameter.feet.estimated_diameter_max)/2).toFixed(2)} ft</p>
                         <p>Run For Our Lives? = {`${object.is_potentially_hazardous_asteroid}`}</p>
                         <a href={object.nasa_jpl_url} >More Info</a>
                     </div>
@@ -50,7 +51,7 @@ class NearEarthObjects extends Component {
                 <div>{this.state.apiDataLoaded ? this.state.objects['2021-02-19'].map((object, index) => (
                     <div key={`Neo ` + index}>
                         <p><strong>Object {index + 1}: {object.name}</strong></p>
-                        <p>Average Diameter: {(object.estimated_diameter.feet.estimated_diameter_min + object.estimated_diameter.feet.estimated_diameter_max)/2} ft</p>
+                        <p>Average Diameter: {((object.estimated_diameter.feet.estimated_diameter_min + object.estimated_diameter.feet.estimated_diameter_max)/2).toFixed(2)} ft</p>
                         <p>Run For Our Lives? = {`${object.is_potentially_hazardous_asteroid}`}</p>
                         <a href={object.nasa_jpl_url} >More Info</a>
                     </div>
